5440cafa44220e5e4fb68f52bb5b61aa57e66577
kwonjoosung <kwonjoosung@gmail.com>
2023-04-03 21:23:27 +0900

    Feat(Entity): 기본 엔티티 추가
    - user: 사용자
    - question : 질문
      - subject : 질문 - 주제

---
+++ src/main/java/com/kwonjs/questioningmusseukgi/domain/question/model/Question.java
@@ -0,0 +1,42 @@
+package com.kwonjs.questioningmusseukgi.domain.question.model;
+
+import static javax.persistence.EnumType.*;
+import static javax.persistence.GenerationType.*;
+import static lombok.AccessLevel.*;
+
+import javax.persistence.Entity;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = PROTECTED)
+@Where(clause = "deleted = false")
+@SQLDelete(sql = "UPDATE question set deleted = true where id=?")
+public class Question {
+
+	@Id
+	@GeneratedValue(strategy = IDENTITY)
+	private Long id;
+
+	@Enumerated(STRING)
+	private Subject subject;
+
+	private String content;
+
+	public Question(Subject subject, String content) {
+		this.subject = subject;
+		this.content = content;
+	}
+
+	public String toTemplate() {
+		return String.format("%s) %s", subject, content);
+	}
+}
