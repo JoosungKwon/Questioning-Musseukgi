b4d0db6e3d1b26c7596f1014ee7cbe388bb3415f
kwonjoosung <kwonjoosung@gmail.com>
2023-04-13 17:10:59 +0900

    feat(Scheduler): 스케줄링 작업 등록
    - 알림 전체 서비스를 스케줄링 작업에 등록
    - 10분 마다 알림 서비스(로직) 동작

--- src/main/java/com/kwonjs/questioningmusseukgi/global/scheduler/Scheduler.java
+++ src/main/java/com/kwonjs/questioningmusseukgi/global/scheduler/Scheduler.java
@@ -1,10 +1,14 @@
 package com.kwonjs.questioningmusseukgi.global.scheduler;
 
+import java.time.LocalTime;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
+
+import com.kwonjs.questioningmusseukgi.domain.notification.NotificationService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -14,10 +18,12 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class Scheduler {
 
+	private final NotificationService notificationService;
+
 	@Async
 	@Scheduled(cron = "0 */10 * * * *", zone = "Asia/Seoul")
 	public void task() {
-
+		notificationService.sendToQuestionAtTime(LocalTime.now());
 	}
 
 }
