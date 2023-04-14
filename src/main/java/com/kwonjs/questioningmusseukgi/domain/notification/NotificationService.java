a5fd55262c646f7787446fdb7e3beb264bb323c1
kwonjoosung <kwonjoosung@gmail.com>
2023-04-15 01:46:42 +0900

    fix(Notification): API 버저닝 및 api path 제거
    - "api/v1" path 제거
    - nginx 설정에 api 추가

--- src/main/java/com/kwonjs/questioningmusseukgi/domain/notification/NotificationService.java
+++ src/main/java/com/kwonjs/questioningmusseukgi/domain/notification/NotificationService.java
@@ -34,14 +34,14 @@ public class NotificationService {
 	public void sendToQuestionAtTime(LocalTime startTime) {
 		// 1. 설정한 알림 시각에 보내야 하는 사용자 정보 가져오기 -> 구독자 정보
 		// 1-1. 얼만큼의 범위의 시간에서 사용자를 식별할 것인가?
-		LocalTime start = startTime;
-		LocalTime finish = start.plusMinutes(RANGE);
+		LocalTime finishTime = startTime.plusMinutes(RANGE);
 
-		List<User> users = userService.getByTime(start, finish);
+		List<User> users = userService.getByTime(startTime, finishTime);
 
 		log.info("users: {}", users);
 
 		// 2. 해당 사용자에게 보낼 질문 고르기 -> 날짜 별로 설정 -> 추후에 사용자 설정에 맞출 수 있도록 확장
+		// multiThread
 		for(User user : users) {
 			Question question = questionService.generateBy(user);
 			// 3. 해당 사용자에게 질문 보내기
--- src/main/java/com/kwonjs/questioningmusseukgi/domain/notification/api/NotificationController.java
+++ src/main/java/com/kwonjs/questioningmusseukgi/domain/notification/api/NotificationController.java
@@ -2,8 +2,6 @@ package com.kwonjs.questioningmusseukgi.domain.notification.api;
 
 import static org.springframework.http.HttpStatus.*;
 
-import java.time.LocalTime;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -15,7 +13,7 @@ import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/notification")
+@RequestMapping("/notification")
 public class NotificationController {
 
 	private final NotificationService notificationService;
@@ -23,7 +21,7 @@ public class NotificationController {
 	@GetMapping("/start")
 	@ResponseStatus(OK)
 	public void startNotificationOnce() { // 스케줄러 수동 동작 임시 메서드 -> 추후에는 사람에 맞게 해당 사람에게 보낼 수 있도록 수정
-		notificationService.sendToQuestionAtTime(LocalTime.now());
+		notificationService.sendToQuestionAtTime();
 	}
 
 }
